f927c17881f15c30e301e45bf45e0ca9f91da60b
Darshanh15 <darshanhgowda5@gmail.com>
2025-07-12 17:24:39 +0530

    commit

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../styles/_gallery.scss';
 
-import img1 from '../assets/gallery/gallery-1.jpg';
-import img2 from '../assets/gallery/gallery-2.jpg';
-import img3 from '../assets/gallery/gallery-3.jpg';
-import img4 from '../assets/gallery/gallery-4.jpg';
-import img5 from '../assets/gallery/gallery-5.jpg';
-import img6 from '../assets/gallery/gallery-6.jpg';
-import img7 from '../assets/gallery/gallery-7.jpg';
-import img8 from '../assets/gallery/gallery-8.jpg';
+import img1 from '../assets/gallery/img (1).jpeg';
+import img2 from '../assets/gallery/img (2).jpeg';
+import img3 from '../assets/gallery/img (3).jpeg';
+import img4 from '../assets/gallery/img (4).jpeg';
+import img5 from '../assets/gallery/img (5).jpeg';
+import img6 from '../assets/gallery/img (6).jpeg';
+import img7 from '../assets/gallery/img (7).jpeg';
+import img8 from '../assets/gallery/img (1).jpeg';
 
 const images = [img1, img2, img3, img4, img5, img6, img7, img8];
 
